15f6eebbae8dc326eb823cc7899a6527c5df3e20
MammutCris <mammutcris@gmail.com>
2017-02-07 11:08:11 +0200

    Create package

---
+++ src/Polyglot.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mammut
+ * Date: 07.02.17
+ * Time: 10:40
+ */
+namespace MammutAlex\Polyglot;
+
+use Illuminate\Foundation\Application;
+/**
+ * Class Polyglot translation model data
+ *
+ * @package App
+ */
+trait Polyglot
+{
+	/**
+	 * Translation data from DB
+	 *
+	 * @param $name
+	 *
+	 * @return mixed
+	 */
+	public function translation($name)
+	{
+		return $this->{$name.'_'.App::getLocale()};
+	}
+}
